4763e98aeffa84b398bd0142dd30e4d6a316bf17
Luthfi Krisna Bayu <132969113+lkbayu@users.noreply.github.com>
2025-02-12 14:22:14 +0700

    Update ikn1.py

--- ikn1.py
+++ ikn1.py
@@ -15,6 +15,8 @@ from gensim.models import LdaModel
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('punkt_tab')
+
 
 def load_data():
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
@@ -28,43 +30,40 @@ def remove_tweet_special(text):
     text = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\\w+:\/\/\\S+)", " ", text).split())
     return text.replace("http://", " ").replace("https://", " ")
 
-def remove_number(text):
-    return re.sub(r"\d+", "", text)
-
-def remove_punctuation(text):
-    return text.translate(str.maketrans("", "", string.punctuation))
-
-def remove_whitespace_LT(text):
-    return text.strip()
-
-def remove_whitespace_multiple(text):
-    return re.sub(r"\s+", " ", text)  
-
-def remove_single_char(text):
-    return re.sub(r"\b[a-zA-Z]\b", "", text)
-
-def stopwords_removal(text, stopwords_list):
-    return " ".join([word for word in text.split() if word not in stopwords_list])
-
-def normalize_text(text, normalization_dict):
-    words = text.split()
-    return " ".join([normalization_dict.get(word, word) for word in words])
-
-def stemming_text(text, stemmer):
-    return stemmer.stem(text)
-
 def preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer):
-    df[text_column] = df[text_column].astype(str).apply(remove_tweet_special)
-    df[text_column] = df[text_column].apply(remove_number)
-    df[text_column] = df[text_column].apply(remove_punctuation)
-    df[text_column] = df[text_column].apply(remove_whitespace_LT)
-    df[text_column] = df[text_column].apply(remove_whitespace_multiple)
-    df[text_column] = df[text_column].apply(remove_single_char)
-    df[text_column] = df[text_column].str.lower()
-    df['cleaned_text'] = df[text_column].apply(lambda x: stopwords_removal(x, stopwords_list))
-    df['cleaned_text'] = df['cleaned_text'].apply(lambda x: normalize_text(x, normalization_dict))
-    df['stemmed_text'] = df['cleaned_text'].apply(lambda x: stemming_text(x, stemmer))
-    df['tokenized_text'] = df['stemmed_text'].apply(word_tokenize)
+    if st.button("Case Cleaning"):
+        df[text_column] = df[text_column].astype(str).apply(remove_tweet_special)
+        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\d+", "", x))
+        df[text_column] = df[text_column].apply(lambda x: x.translate(str.maketrans("", "", string.punctuation)))
+        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\s+", " ", x).strip())
+        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\b[a-zA-Z]\b", "", x))
+        st.write("Case Cleaning Done!", df.head())
+    
+    if st.button("Case Folding"):
+        df[text_column] = df[text_column].str.lower()
+        st.write("Case Folding Done!", df.head())
+    
+    if st.button("Stopword Removal"):
+        df[text_column] = df[text_column].apply(lambda x: " ".join([word for word in x.split() if word not in stopwords_list]))
+        st.write("Stopword Removal Done!", df.head())
+    
+    if st.button("Normalization"):
+        df[text_column] = df[text_column].apply(lambda x: " ".join([normalization_dict.get(word, word) for word in x.split()]))
+        st.write("Normalization Done!", df.head())
+    
+    if st.button("Stemming"):
+        df[text_column] = df[text_column].apply(lambda x: stemmer.stem(x))
+        st.write("Stemming Done!", df.head())
+    
+    if st.button("Tokenization"):
+        df['tokenized_text'] = df[text_column].apply(word_tokenize)
+        st.write("Tokenization Done!", df[['tokenized_text']].head())
+    
+    if st.button("Bag of Words (BoW)"):
+        dictionary = corpora.Dictionary(df['tokenized_text'])
+        bow_corpus = [dictionary.doc2bow(text) for text in df['tokenized_text']]
+        st.write("Bag of Words Done!", dictionary.token2id)
+    
     return df
 
 def main():
@@ -72,12 +71,12 @@ def main():
     df = load_data()
     if df is not None:
         text_column = st.selectbox("Pilih kolom teks", df.columns)
-        if st.button("Proses Data"):
-            stopwords_list = set(stopwords.words('indonesian'))
-            normalization_dict = {}
-            stemmer = StemmerFactory().create_stemmer()
-            df = preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer)
-            st.write("Data setelah preprocessing:", df.head())
+        stopwords_list = set(stopwords.words('indonesian'))
+        normalization_dict = {}
+        stemmer = StemmerFactory().create_stemmer()
+        df = preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer)
+        
+        if st.button("Modelling LDA"):
             dictionary = corpora.Dictionary(df['tokenized_text'])
             bow_corpus = [dictionary.doc2bow(text) for text in df['tokenized_text']]
             lda_model = LdaModel(bow_corpus, num_topics=5, id2word=dictionary, passes=10, alpha=0.5, random_state=37)
@@ -85,8 +84,10 @@ def main():
             st.write("Topik yang ditemukan:")
             for topic in topics:
                 st.write(topic)
+        
+        if st.button("Wordcloud"):
             plt.figure(figsize=(15, 12))
-            for i, topic in enumerate(topics):
+            for i, topic in enumerate(lda_model.print_topics(num_words=10)):
                 ax = plt.subplot(3, 3, i+1)
                 words = topic[1]
                 word_freq = {word.split('*')[1].strip().strip('"'): float(word.split('*')[0]) for word in words.split(' + ')}
@@ -94,13 +95,13 @@ def main():
                 ax.imshow(wordcloud, interpolation='bilinear')
                 ax.set_title(f'Topik {i+1}')
                 ax.axis('off')
-                for spine in ax.spines.values():
-                    spine.set_edgecolor('black')
-                    spine.set_linewidth(2)
             plt.tight_layout()
             st.pyplot(plt)
+        
+        if st.button("pyLDAvis"):
             lda_vis_data = gensimvis.prepare(lda_model, bow_corpus, dictionary, sort_topics=False)
             pyLDAvis_html = pyLDAvis.prepared_data_to_html(lda_vis_data)
             st.components.v1.html(pyLDAvis_html, width=1300, height=800)
+
 if __name__ == "__main__":
     main()
