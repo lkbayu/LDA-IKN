f511434a2d35f1f0e1acd69e1443893e50180e3b
Luthfi Krisna Bayu <132969113+lkbayu@users.noreply.github.com>
2025-02-12 14:50:10 +0700

    Update ikn1.py

--- ikn1.py
+++ ikn1.py
@@ -17,7 +17,6 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
-
 def load_data():
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
     if uploaded_file is not None:
@@ -31,39 +30,17 @@ def remove_tweet_special(text):
     return text.replace("http://", " ").replace("https://", " ")
 
 def preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer):
-    if st.button("Case Cleaning"):
-        df[text_column] = df[text_column].astype(str).apply(remove_tweet_special)
-        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\d+", "", x))
-        df[text_column] = df[text_column].apply(lambda x: x.translate(str.maketrans("", "", string.punctuation)))
-        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\s+", " ", x).strip())
-        df[text_column] = df[text_column].apply(lambda x: re.sub(r"\b[a-zA-Z]\b", "", x))
-        st.write("Case Cleaning Done!", df.head())
-    
-    if st.button("Case Folding"):
-        df[text_column] = df[text_column].str.lower()
-        st.write("Case Folding Done!", df.head())
-    
-    if st.button("Stopword Removal"):
-        df[text_column] = df[text_column].apply(lambda x: " ".join([word for word in x.split() if word not in stopwords_list]))
-        st.write("Stopword Removal Done!", df.head())
-    
-    if st.button("Normalization"):
-        df[text_column] = df[text_column].apply(lambda x: " ".join([normalization_dict.get(word, word) for word in x.split()]))
-        st.write("Normalization Done!", df.head())
-    
-    if st.button("Stemming"):
-        df[text_column] = df[text_column].apply(lambda x: stemmer.stem(x))
-        st.write("Stemming Done!", df.head())
-    
-    if st.button("Tokenization"):
-        df['tokenized_text'] = df[text_column].apply(word_tokenize)
-        st.write("Tokenization Done!", df[['tokenized_text']].head())
-    
-    if st.button("Bag of Words (BoW)"):
-        dictionary = corpora.Dictionary(df['tokenized_text'])
-        bow_corpus = [dictionary.doc2bow(text) for text in df['tokenized_text']]
-        st.write("Bag of Words Done!", dictionary.token2id)
-    
+    df[text_column] = df[text_column].astype(str).apply(remove_tweet_special)
+    df[text_column] = df[text_column].apply(lambda x: re.sub(r"\d+", "", x))
+    df[text_column] = df[text_column].apply(lambda x: x.translate(str.maketrans("", "", string.punctuation)))
+    df[text_column] = df[text_column].apply(lambda x: x.strip())
+    df[text_column] = df[text_column].apply(lambda x: re.sub(r"\s+", " ", x))
+    df[text_column] = df[text_column].apply(lambda x: re.sub(r"\b[a-zA-Z]\b", "", x))
+    df[text_column] = df[text_column].str.lower()
+    df['cleaned_text'] = df[text_column].apply(lambda x: " ".join([word for word in x.split() if word not in stopwords_list]))
+    df['cleaned_text'] = df['cleaned_text'].apply(lambda x: " ".join([normalization_dict.get(word, word) for word in x.split()]))
+    df['stemmed_text'] = df['cleaned_text'].apply(lambda x: stemmer.stem(x))
+    df['tokenized_text'] = df['stemmed_text'].apply(word_tokenize)
     return df
 
 def main():
@@ -71,10 +48,17 @@ def main():
     df = load_data()
     if df is not None:
         text_column = st.selectbox("Pilih kolom teks", df.columns)
-        stopwords_list = set(stopwords.words('indonesian'))
-        normalization_dict = {}
-        stemmer = StemmerFactory().create_stemmer()
-        df = preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer)
+        if st.button("Preprocessing"):
+            stopwords_list = set(stopwords.words('indonesian'))
+            normalization_dict = {}
+            stemmer = StemmerFactory().create_stemmer()
+            df = preprocess_text(df, text_column, stopwords_list, normalization_dict, stemmer)
+            st.write("Data setelah preprocessing:", df.head())
+        
+        if st.button("Bag of Words (BoW)"):
+            dictionary = corpora.Dictionary(df['tokenized_text'])
+            bow_corpus = [dictionary.doc2bow(text) for text in df['tokenized_text']]
+            st.write("Contoh BoW:", bow_corpus[:5])
         
         if st.button("Modelling LDA"):
             dictionary = corpora.Dictionary(df['tokenized_text'])
@@ -86,8 +70,9 @@ def main():
                 st.write(topic)
         
         if st.button("Wordcloud"):
+            topics = lda_model.print_topics(num_words=10)
             plt.figure(figsize=(15, 12))
-            for i, topic in enumerate(lda_model.print_topics(num_words=10)):
+            for i, topic in enumerate(topics):
                 ax = plt.subplot(3, 3, i+1)
                 words = topic[1]
                 word_freq = {word.split('*')[1].strip().strip('"'): float(word.split('*')[0]) for word in words.split(' + ')}
@@ -95,10 +80,13 @@ def main():
                 ax.imshow(wordcloud, interpolation='bilinear')
                 ax.set_title(f'Topik {i+1}')
                 ax.axis('off')
+                for spine in ax.spines.values():
+                    spine.set_edgecolor('black')
+                    spine.set_linewidth(2)
             plt.tight_layout()
             st.pyplot(plt)
         
-        if st.button("pyLDAvis"):
+        if st.button("Visualisasi pyLDAvis"):
             lda_vis_data = gensimvis.prepare(lda_model, bow_corpus, dictionary, sort_topics=False)
             pyLDAvis_html = pyLDAvis.prepared_data_to_html(lda_vis_data)
             st.components.v1.html(pyLDAvis_html, width=1300, height=800)
